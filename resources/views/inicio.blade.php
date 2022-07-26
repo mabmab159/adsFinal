1f1e9c37bebc70bc6c28c8ccdc07f97351e06cb7
MabMab^^ <miguelberrioh@hotmail.com>
2022-07-26 07:21:52 -0500

    Cambios

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -8,7 +8,9 @@
     <title>Login Gozu</title>
     <link rel="icon" href="{{ asset('static/hotel.ico') }}"/>
     <link rel="stylesheet" href="{{ asset('static/login.css') }}"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+          integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
 </head>
 <body>
 <form class="login" method="post" action="{{route("login")}}">
@@ -16,18 +18,18 @@
     <div class="login-box">
         <h1 class="loginTitle">Bienvenido</h1>
         <div class="text-box">
-        <i class="fa-solid fa-user"></i>
+            <i class="fa-solid fa-user"></i>
             <input name="usuario" placeholder="Ingrese usuario"/>
         </div>
-    
+
         <div class="text-box">
-        <i class="fa-solid fa-lock"></i>
-            <input name="password" placeholder="Ingrese password" type="password" />
+            <i class="fa-solid fa-lock"></i>
+            <input name="password" placeholder="Ingrese password" type="password"/>
         </div>
-        
+
         <button class="btn">Iniciar Sesión</button>
     </div>
-    
+
 </form>
 
 </body>
