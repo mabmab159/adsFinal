b88bf8e2d20988756e8b929199a01ba7a5dfa998
AngeloJampier <angelo.20jcp@gmail.com>
2022-07-26 04:05:12 -0500

    formulario usuarios

--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -3,84 +3,91 @@
     @if(isset($usuario))
         <form method="post" action="{{route("creandoUsuario")}}">
             @csrf
-            <div style="display: none">
-                <label>id</label>
-                <input name="id" value="{{$usuario->id}}">
+            <div class="usuario-box">
+                <div class="textBox">
+                    <label>id</label>
+                    <input name="id" value="{{$usuario->id}}">
+                </div>
+                <div class="textBox">
+                    <label>Nombre</label>
+                    <input name="nombre" value="{{$usuario->nombre}}">
+                </div>
+                <div class="textBox">
+                    <label>Apellido</label>
+                    <input name="apellido" value="{{$usuario->apellido}}">
+                </div>
+                <div>
+                    <label>Cargo</label>
+                    <select name="cargo">
+                        @if($usuario->cargo == "administrador")
+                            <option value="administrador" selected>Administrador</option>
+                        @else
+                            <option value="administrador">Administrador</option>
+                        @endif
+                        @if($usuario->cargo == "recepcionista")
+                            <option value="recepcionista" selected>Recepcionista</option>
+                        @else
+                            <option value="recepcionista">Recepcionista</option>
+                        @endif
+                        @if($usuario->cargo == "conserje")
+                            <option value="conserje" selected>Conserje</option>
+                        @else
+                            <option value="conserje">Conserje</option>
+                        @endif
+                    </select>
+                </div>
+                <div>
+                    <label>Usuario</label>
+                    <input name="usuario" value="{{$usuario->usuario}}">
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input name="password">
+                </div>
+                <div>
+                    <button type="submit">Guardar</button>
+                </div>
             </div>
-            <div>
-                <label>Nombre</label>
-                <input name="nombre" value="{{$usuario->nombre}}">
-            </div>
-            <div>
-                <label>Apellido</label>
-                <input name="apellido" value="{{$usuario->apellido}}">
-            </div>
-            <div>
-                <label>Cargo</label>
-                <select name="cargo">
-                    @if($usuario->cargo == "administrador")
-                        <option value="administrador" selected>Administrador</option>
-                    @else
-                        <option value="administrador">Administrador</option>
-                    @endif
-                    @if($usuario->cargo == "recepcionista")
-                        <option value="recepcionista" selected>Recepcionista</option>
-                    @else
-                        <option value="recepcionista">Recepcionista</option>
-                    @endif
-                    @if($usuario->cargo == "conserje")
-                        <option value="conserje" selected>Conserje</option>
-                    @else
-                        <option value="conserje">Conserje</option>
-                    @endif
-                </select>
-            </div>
-            <div>
-                <label>Usuario</label>
-                <input name="usuario" value="{{$usuario->usuario}}">
-            </div>
-            <div>
-                <label>Password</label>
-                <input name="password">
-            </div>
-            <div>
-                <button type="submit">Guardar</button>
-            </div>
+            
         </form>
     @else
         <form method="post" action="{{route("creandoUsuario")}}">
             @csrf
-            <div style="display: none">
-                <label>id</label>
-                <input name="id" value="0">
+            <div class="usuario-box">
+                <div class="textBox">
+                    <label class="textId">id:</label>
+                    <input name="id" value="0" placeholder="Ingrese id">
+                </div>
+                <div class="textBox">
+                    <label>Nombre:</label>
+                    <input name="nombre" placeholder="Ingrese nombre">
+                </div>
+                <div class="textBox">
+                    <label>Apellido:</label>
+                    <input name="apellido" placeholder="Ingrese apellido">
+                </div>
+                <div>
+                    <label>Cargo</label>
+                    <select name="cargo">
+                        <option value="administrador">Administrador</option>
+                        <option value="recepcionista">Recepcionista</option>
+                        <option value="conserje">Conserje</option>
+                    </select>
+                </div>
+                <div>
+                    <label>Usuario</label>
+                    <input name="usuario">
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input name="password">
+                </div>
+                <div>
+                    <button type="submit">Registrar</button>
+                </div>
+                
             </div>
-            <div>
-                <label>Nombre</label>
-                <input name="nombre">
-            </div>
-            <div>
-                <label>Apellido</label>
-                <input name="apellido">
-            </div>
-            <div>
-                <label>Cargo</label>
-                <select name="cargo">
-                    <option value="administrador">Administrador</option>
-                    <option value="recepcionista">Recepcionista</option>
-                    <option value="conserje">Conserje</option>
-                </select>
-            </div>
-            <div>
-                <label>Usuario</label>
-                <input name="usuario">
-            </div>
-            <div>
-                <label>Password</label>
-                <input name="password">
-            </div>
-            <div>
-                <button type="submit">Registrar</button>
-            </div>
+            
         </form>
     @endif
     <p>Aca va el formulario para que se creen nuevos usuarios prro</p>
